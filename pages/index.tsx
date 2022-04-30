667d3ce32d205cab2e1e41aee42195829b974ad9
Teerut Srithongdee <46746401+Teerut26@users.noreply.github.com>
2022-04-30 20:44:00 +0700

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store/root";
+import { countActions } from "../store/slice/counterSlice";
+
+const Index: React.FC = () => {
+    const count = useSelector((state: RootState) => state.countSlice.count);
+    const dispatch = useDispatch()
+    return (
+        <div className="absolute top-0 bottom-0 right-0 left-0 flex flex-col justify-center items-center">
+            <div>{count}</div>
+            <button onClick={()=>dispatch(countActions.increase())}>increase</button>
+            <button onClick={()=>dispatch(countActions.decrease())}>decrease</button>
+        </div>
+    );
+};
+
+export default Index;
